5bd74e9d4c18e3806abfa620ffc7f004f32ac736
Austin FitzGerald <fitzgerald.austind@gmail.com>
2017-02-09 08:44:45 -0600

    updated g3

--- assets/js/galleries.js
+++ assets/js/galleries.js
@@ -51,3 +51,33 @@ $('#g2').magnificPopup({
   }
 });
 $('#g2').css('background-image', 'url(./assets/albums/g2/thumb.png)');
+
+
+$('#g3').magnificPopup({
+    items: [
+      {
+        src: './assets/albums/g3/1.png'
+      },
+      {
+        src: './assets/albums/g3/2.png'
+      },
+      {
+        src: './assets/albums/g3/3.png'
+      },
+            {
+        src: './assets/albums/g3/4.png'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image',
+  callbacks: {
+    
+    buildControls: function() {
+      this.contentContainer.append(this.arrowLeft.add(this.arrowRight));
+    }
+    
+  }
+});
+$('#g2').css('background-image', 'url(./assets/albums/g3/thumb.png)');
